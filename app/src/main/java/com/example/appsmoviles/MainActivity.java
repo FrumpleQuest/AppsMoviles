89970dc922e48a3b5f2a62fd5f8a32e5b19ad34f
Eric <eric.durante@usm.cl>
2022-05-10 02:50:33 -0400

    New

--- app/src/main/java/com/example/appsmoviles/MainActivity.java
+++ app/src/main/java/com/example/appsmoviles/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.appsmoviles;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     //HOLA SOY EL TON
-
+    Button siguiente, configuracion, archivos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +24,32 @@ public class MainActivity extends AppCompatActivity {
                 new SQLiteHelper(this, "DBUsuarios", null, 1);
 
         SQLiteDatabase db = usdbh.getWritableDatabase();
+
+        siguiente=(Button)findViewById(R.id.siguiente);
+        siguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent( MainActivity.this, recordatorios.class);
+                startActivity(i);
+            }
+        });
+
+        configuracion=(Button)findViewById(R.id.configuracion);
+        configuracion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent j = new Intent( MainActivity.this, configuracion.class);
+                startActivity(j);
+            }
+        });
+
+        archivos=(Button)findViewById(R.id.archivos);
+        archivos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent z = new Intent( MainActivity.this, archivos.class);
+                startActivity(z);
+            }
+        });
     }
 }
