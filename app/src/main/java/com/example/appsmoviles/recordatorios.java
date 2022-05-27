92d3b381bdaa70015f3dd25a919081c9ab0fb239
Eric <eric.durante@usm.cl>
2022-05-26 21:10:38 -0400

    Jueves 21:10

--- app/src/main/java/com/example/appsmoviles/recordatorios.java
+++ app/src/main/java/com/example/appsmoviles/recordatorios.java
@@ -11,26 +11,29 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 public class recordatorios extends AppCompatActivity {
 
     ListView mylistview;
 
-    String[] l_recordatorios = {"Lunes 3, Llevar al veterinario", "Lunes 10, poner vacuna", "Lunes 17, Esterilizar"};
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recordatorios);
-
+        mylistview = (ListView) findViewById(R.id.rv1);
 
         //Abrimos la base de datos 'DBUsuarios' en modo lectura-escritura
         SQLiteHelper usdbh = new SQLiteHelper(this, "DBUsuarios", null, 1);
@@ -41,6 +44,33 @@ public class recordatorios extends AppCompatActivity {
         c.moveToFirst();
 
         //LLenamos la lista con la BD (Aqui habría que tener 3 listas para los 3 atributos)
+        HashMap<String, String> nameAddresses = new HashMap<>();
+        boolean flag = true;
+        while(flag){
+            nameAddresses.put(c.getString(2) + ", " + c.getString(4), c.getString(3));
+            if (c.isLast()){
+                flag = false;
+            }
+            c.moveToNext();
+        }
+
+        List<HashMap<String, String>> lisitems= new ArrayList<>();
+        SimpleAdapter adapter = new SimpleAdapter(this,
+                lisitems,
+                android.R.layout.simple_list_item_2,
+                new String[]{"first", "second"},
+                new int[]{android.R.id.text1, android.R.id.text2}
+        );
+        Iterator it = nameAddresses.entrySet().iterator();
+        while(it.hasNext()){
+            HashMap<String, String> resultMap = new HashMap<>();
+            Map.Entry pair = (Map.Entry)it.next();
+            resultMap.put("first", pair.getKey().toString());
+            resultMap.put("second", pair.getValue().toString());
+            lisitems.add(resultMap);
+        }
+
+        /*
         LinkedList<String> TitulosFechas = new LinkedList<String>();
         LinkedList<String> Subtitulos = new LinkedList<String>();
         boolean flag = true;
@@ -60,7 +90,7 @@ public class recordatorios extends AppCompatActivity {
                 android.R.layout.simple_list_item_1,
                 TitulosFechas
         );
-
+*/
         mylistview.setAdapter(adapter);
 
     }
